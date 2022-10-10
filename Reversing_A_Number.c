eb53b1ea28016f4157daa1222cc00b6f283c8bd8
Jakki_Rajasekhar_Ramana <114324888+JakkiRajasekharRamana@users.noreply.github.com>
2022-10-10 23:03:19 +0530

    Add files via upload

---
+++ Reversing_A_Number.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int num,x;
+    printf("Enter Your Number:");
+    scanf("%d",&num);
+    
+    x=num%10;
+    printf("%d",x);
+    
+    num=num/10;
+    x=num%10;
+    printf("%d",x);
+    
+    num=num/10;
+    x=num%10;
+    printf("%d",x);
+    
+    num=num/10;
+    x=num%10;
+    printf("%d",x);
+    
+   
+
+}
+
+
+
