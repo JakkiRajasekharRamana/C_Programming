4e8e24d9f5016d6f1ecce2bc0d151ee08c5ada48
Jakki_Rajasekhar_Ramana <114324888+JakkiRajasekharRamana@users.noreply.github.com>
2022-10-17 21:58:05 +0530

    Add files via upload

---
+++ sum_of_first_N_even_numbers.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a=0;
+    int d=2;
+    int sum,n,num1;
+    
+    printf("Enter A Number such that, sum of first n even numbers is,then n :");
+    scanf("%d",&n);
+    
+    num1=(2*a+(n-1)*d)*n;
+    sum=num1/2;
+
+    printf("Sum of first n even numbers is=%d",sum);
+    
+    
+}
+
