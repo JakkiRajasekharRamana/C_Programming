2fd921e5bfdef40f93cda38b0e138980a1026513
Jakki_Rajasekhar_Ramana <114324888+JakkiRajasekharRamana@users.noreply.github.com>
2022-11-09 22:33:59 +0530

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+
+int main()
+{
+    double num1;
+    double num2;
+    char op;
+    
+    printf("Enter Your first number :");
+    scanf("%lf",&num1);
+    
+    printf("Enter Your Operator :");
+    scanf(" %c",&op);
+    
+    printf("Enter Your secound number :");
+    scanf("%lf",&num2);
+    
+    
+    if (op == '+')
+    {   
+        printf("%f",num1+num2);
+    }
+    else if (op == '-')
+    {
+        printf("%f",num1-num2);
+    }
+    else if(op == '*')
+    {
+        printf("%f",num1 * num2);
+    }
+    else if(op == '/')
+    {
+        printf("%f",num1 / num2);
+    }
+    else 
+    {
+        printf("invalid operation");
+    }
+
+}
+
+
+
