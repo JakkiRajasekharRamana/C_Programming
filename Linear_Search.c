775532ee5153dddf0297de052f8265d76c099d20
Jakki_Rajasekhar_Ramana <114324888+JakkiRajasekharRamana@users.noreply.github.com>
2023-08-02 17:41:37 +0530

    Add files via upload

---
+++ Linear_Search.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int array[6]={2,6,1,9,1,5};
+    for(int i=0;i<6;i++){
+      if(a==array[i])
+        printf("yea");
+      
+}
+}
